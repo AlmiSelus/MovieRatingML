42b20dbe59d090ce7bc2ea28cb8674b92f745493
Stilos <stilos.dev@gmail.com>
2017-03-23 16:42:51 +0100

    Front-End - Some ERRORS fixes.

---
+++ MovieRatingML-Frontend/src/app/components/navigation/navigation.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nav-component',
+  temlplateUrl: 'app/components/navigation/navigation.component.html'
+})
+
+export class NavigationComponent {
+  /*private navigationLinks: string[] = ["Home", "Movies", "About Us", "Contact"];*/
+}
